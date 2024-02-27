8e39255816e0c489252473616f4c7b56d3cf56c3
VibhorSingh-Bisht <101184029+VibhorSingh-Bisht@users.noreply.github.com>
2024-02-27 12:40:47 +0530

    pdf.py

--- pdf.py
+++ pdf.py
@@ -1,7 +1,7 @@
 #pdf.py
 import os
-from llama_index import StorageContext, VectorStoreIndex, load_index_from_storage
-from llama_index.readers import PDFReader
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.readers.file import PDFReader
 
 
 
